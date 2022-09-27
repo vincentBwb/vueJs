0afcae5f5aaaadd622c4af242b609fe6755302b7
vincentBwb <20cent.bwb@gmail.com>
2022-09-27 15:55:00 +0200

    ajouts de fichiers

---
+++ parent-enfant/ChildComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+<script>
+    export default {
+        name: Child ,
+        props: {
+            message: string, 
+        }
+    }
+</script>
+
+</body>
+</html>
